d77d28b24de77d9321300fb95a931097edc02d7e
jacobdpete <jacobdpete@gmail.com>
2026-03-03 21:20:20 -0500

    Quartz sync: Mar 3, 2026, 9:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jacobdpete.github.io/digital-garden",
+    baseUrl: "jacobdpete.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
