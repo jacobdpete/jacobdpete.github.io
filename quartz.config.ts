a4f35cbe363d6a561501e5e82187d962ce63987a
jacobdpete <jacobdpete@gmail.com>
2026-03-09 15:14:46 -0400

    Quartz sync: Mar 9, 2026, 3:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,6 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(), /* <--- THE PRODIGAL SON RETURNS */
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
