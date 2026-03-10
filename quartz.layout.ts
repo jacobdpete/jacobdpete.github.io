540fbaf20bd09ca9144b60c0cf650146099e69a6
jacobdpete <jacobdpete@gmail.com>
2026-03-10 18:51:45 -0400

    Quartz sync: Mar 10, 2026, 6:51 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,8 +17,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.MenuBar(), /* <--- Your new menu bar goes here! */
-    Component.Breadcrumbs(),
+    Component.MenuBar(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
